1ce87a72e2c514ad9835d128d5b6449ea20f1ccc
liam027 <liam027@gmail.com>
2020-09-17 14:34:05 -0400

    Add cors package

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { response } = require('express')
 const express = require('express');
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express();
 
+// Define middleware
+app.use(cors())
 app.use(express.json());
 app.use(morgan('tiny'))
 
