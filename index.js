6bb9e258e48cd31b21586a62b8f558549d68ed9a
liam027 <liam027@gmail.com>
2020-09-17 14:33:58 -0400

    Refactor for products instead of notes

--- index.js
+++ index.js
@@ -7,72 +7,74 @@ const app = express();
 app.use(express.json());
 app.use(morgan('tiny'))
 
-let notes = [{
-  id: 1,
-  content: "HTML is easy",
-  date: "2019-05-30T17:30:31.098Z",
-  important: true
-},
-{
-  id: 2,
-  content: "Browser can execute only Javascript",
-  date: "2019-05-30T18:39:34.091Z",
-  important: false
-},
-{
-  id: 3,
-  content: "GET and POST are the most important methods of HTTP protocol",
-  date: "2019-05-30T19:20:14.298Z",
-  important: true
-}];
+let products = [
+    {
+      "id": 1,
+      "name": "turkey",
+      "category": "produce",
+      "quantity": 0,
+    },
+    {
+      "id": 2,
+      "name": "rice",
+      "category": "produce",
+      "quantity": 0
+    },
+    {
+      "id": 3,
+      "name": "vinegar",
+      "category": "produce",
+      "quantity": 0
+    }
+  ]
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
+app.get('/api/products', (req, res) => {
+  res.json(products);
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const id = Number(req.params.id);
-  const note = notes.find(note => note.id === id);
-  if (note) {
-    res.json(note)
+  const product = products.find(product => product.id === id);
+  if (product) {
+    res.json(product)
   } else {
     res.status(404).end()
   }
 })
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/products', (req, res) => {
   const body = req.body
 
-  if (!body.content) {
+  if (!body) {
     return res.status(400).json({
       error: 'content missing'
     })
   }
 
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+  const product = {
     id: generateId(),
+    name: body.name,
+    category: body.category || undefined,
+    quantity: body.quantity,
+    created_at: new Date()
   }
 
-  notes = notes.concat(note)
+  products = products.concat(product)
 
-  console.log("Added new note: ", note);
+  console.log("Added new product: ", product);
 
-  res.json(note)
+  res.json(product)
 })
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/products/:id', (req, res) => {
   const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-
+  products = products.filter(product => product.id !== id)
+  console.log("Deleted product with ID: ", id);
   res.status(204).end()
 })
 
@@ -83,8 +85,8 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = products.length > 0
+    ? Math.max(...products.map(n => n.id))
     : 0
   return maxId + 1
 }
