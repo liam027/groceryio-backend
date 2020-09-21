33e18379e6c34d6b6d4f8f44f6938098d8b344fa
liam027 <liam027@gmail.com>
2020-09-21 14:24:02 -0400

    Refactor reused code into test_helper module

--- tests/product_api.test.js
+++ tests/product_api.test.js
@@ -1,40 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Product = require('../models/product')
 
-const initialProducts = [
-  {
-    'name': 'turkey',
-    'category': 'produce',
-    'quantity': 0,
-    'created_at': new Date()
-  },
-  {
-    'name': 'rice',
-    'category': 'produce',
-    'quantity': 0,
-    'created_at': new Date()
-  },
-  {
-    'name': 'vinegar',
-    'category': 'produce',
-    'quantity': 0,
-    'created_at': new Date()
-  }
-]
-
 beforeEach(async () => {
   await Product.deleteMany({})
 
-  let productObject = new Product(initialProducts[0])
+  let productObject = new Product(helper.initialProducts[0])
   await productObject.save()
 
-  productObject = new Product(initialProducts[1])
+  productObject = new Product(helper.initialProducts[1])
   await productObject.save()
 
-  productObject = new Product(initialProducts[2])
+  productObject = new Product(helper.initialProducts[2])
   await productObject.save()
 })
 
@@ -48,7 +28,7 @@ test('products are returned as json', async () => {
 test('all products are returned', async () => {
   const response = await api.get('/api/products')
 
-  expect(response.body).toHaveLength(initialProducts.length)
+  expect(response.body).toHaveLength(helper.initialProducts.length)
 })
 
 test('a specific product is within the returned products', async () => {
@@ -57,6 +37,44 @@ test('a specific product is within the returned products', async () => {
   const names = response.body.map(product => product.name)
 
   expect(names).toContain('rice')
+})
+
+test('a valid product can be added', async () => {
+  const testProduct = {
+    name: 'async',
+    category: 'frozen',
+    quantity: 999
+  }
+
+  await api
+    .post('/api/products')
+    .send(testProduct)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  // Product has been added
+  const productsAtEnd = await helper.productsInDb()
+  expect(productsAtEnd).toHaveLength(helper.initialProducts.length + 1)
+
+  // The new product information is present
+  const contents = productsAtEnd.map(product => product.name)
+  expect(contents).toContain('async')
+})
+
+test('product without name is not added', async () => {
+  const testProduct = {
+    name: '',
+    category: 'frozen',
+    quantity: 999
+  }
+
+  await api
+    .post('/api/products')
+    .send(testProduct)
+    .expect(400)
+
+  const productsAtEnd = await helper.productsInDb()
+  expect(productsAtEnd).toHaveLength(helper.initialProducts.length)
 })
 
 afterAll(() => {
---
+++ tests/test_helper.js
@@ -0,0 +1,39 @@
+const Product = require('../models/product')
+
+const initialProducts = [
+  {
+    'name': 'turkey',
+    'category': 'produce',
+    'quantity': 0,
+    'created_at': new Date()
+  },
+  {
+    'name': 'rice',
+    'category': 'produce',
+    'quantity': 0,
+    'created_at': new Date()
+  },
+  {
+    'name': 'vinegar',
+    'category': 'produce',
+    'quantity': 0,
+    'created_at': new Date()
+  }
+]
+
+const nonExistingId = async () => {
+  const product = new Product({ content: 'willremovethissoon', date: new Date() })
+  await product.save()
+  await product.remove()
+
+  return product._id.toString()
+}
+
+const productsInDb = async () => {
+  const products = await Product.find({})
+  return products.map(product => product.toJSON())
+}
+
+module.exports = {
+  initialProducts, nonExistingId, productsInDb
+}
