01356d97ff9af5e9e3f72dbed7e5754f1c75ec7f
liam027 <liam027@gmail.com>
2020-09-21 15:23:02 -0400

    Add User tests (first pass)

---
+++ tests/models/user_test.test.js
@@ -0,0 +1,45 @@
+const app = require('../../app')
+const bcrypt = require('bcrypt')
+const helper = require('../test_helper')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const User = require('../../models/user')
+
+const api = supertest(app)
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('password', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'new-user',
+      email: 'test@test.com',
+      password: 'password',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
